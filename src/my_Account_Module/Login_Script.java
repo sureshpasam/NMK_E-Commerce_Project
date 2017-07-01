c4c2f39cd78ba3ef6a66a3c18669487e2953dc18
sravan <sravan@10.1.10.18>
2017-07-01 13:48:41 -0700

    Login script modified

--- src/my_Account_Module/Login_Script.java
+++ src/my_Account_Module/Login_Script.java
@@ -1,8 +1,6 @@
 package my_Account_Module;
-
 import org.testng.annotations.Test;
-import org.testng.annotations.BeforeMethod;
-
+import org.testng.annotations.BeforeMethod
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -11,12 +9,15 @@ import org.testng.annotations.AfterMethod;
 public class Login_Script {
 	FirefoxDriver dr;	
   @Test
+  
+  // login test
   public void Validlogintest() {
 	  dr.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
 	  dr.findElement(By.id("email")).sendKeys("psuresh.qatest@gmail.com");
 	  dr.findElement(By.id("password")).sendKeys("suresh@123");  
 	  dr.findElement(By.xpath("/html/body/table/tbody/tr[2]/td/table/tbody/tr/td[2]/table/tbody/tr[4]/td[2]/form/table/tbody/tr[5]/td/a/div")).click();
   }
+  // launch the broswer
   @BeforeMethod
   public void setup() {
 	  dr= new FirefoxDriver();
